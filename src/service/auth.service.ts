f14cecbcc0f63c2f242dd686af93f7764ae0788b
Arthur L <31710356+arthur-lemaire@users.noreply.github.com>
2024-01-27 18:07:47 +0100

    feat(token) : sauvegarde du token dans le localstorage

--- src/service/auth.service.ts
+++ src/service/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { GoogleAuthProvider } from '@angular/fire/auth';
+import { GoogleAuthProvider, User } from '@angular/fire/auth';
 
 
 @Injectable({
@@ -10,10 +10,24 @@ export class AuthService {
   constructor(private afAuth: AngularFireAuth, private googleAuthProvider : GoogleAuthProvider ) {}
 
   signInWithGoogle() {
-    return this.afAuth.signInWithPopup(this.googleAuthProvider);
+    return this.afAuth.signInWithPopup(this.googleAuthProvider).then((result) => {
+      // Stocker le token dans le local storage
+      this.setTokenInLocalStorage(result.user as User);
+    });
   }
 
   signOut() {
-    return this.afAuth.signOut();
+    return this.afAuth.signOut()
+      .then(() => {
+        // Supprimer le token du local storage lors de la déconnexion
+        localStorage.removeItem('userToken');
+      });
+  }
+
+  private setTokenInLocalStorage(user: User) {
+    user.getIdToken().then((token) => {
+      // Stocker le token dans le local storage
+      localStorage.setItem('userToken', token);
+    });
   }
 }
